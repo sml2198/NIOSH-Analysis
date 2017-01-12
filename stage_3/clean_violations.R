b0fa2496a925e04fa27c8a77abdb148be800b80c
sml2198 <sarah.michael.levine@gmail.com>
2017-01-11 23:26:00 -0800

    adds skeleton clean files for stage three

---
+++ stage_3/clean_violations.R
@@ -0,0 +1,46 @@
+# NIOSH Project 2014-N-15776
+# Designing a Statistical Algorithm for Strategic Identification and Development 
+# of New Mine Safety Technologies and Technological Applications
+
+# Primary Investigator: Alison Morantz, amorantz@law.stanford.edu
+
+#  - Clean Violations
+  # Cleans violations
+
+# Coded by Sarah Levine, sarah.michael.levine@gmail.com
+# Last edit 1/11/17
+
+################################################################################
+
+library()
+
+################################################################################
+
+# set root directory
+# root = "/NIOSH-Analysis/data"
+root = "/Users/Sarah/Dropbox (Stanford Law School)/NIOSH/NIOSH-Analysis/data"
+# root = "C:/Users/jbodson/Dropbox (Stanford Law School)/NIOSH/NIOSH-Analysis/data"
+
+# define file paths
+originals.path = paste0(root, "/0_originals", collapse = NULL) 
+clean.path = paste0(root, "/1_cleaned", collapse = NULL) 
+
+# inputs
+
+# outputs
+
+# create file paths (recursive = TRUE will create this file structure if it does not exist)
+dir.create(clean.path, recursive = TRUE)
+
+################################################################################
+
+################################################################################
+
+# output mine-level data
+saveRDS(, file = )
+
+################################################################################
+
+rm(list = ls())
+
+################################################################################
