d15a048587464a922bea856f5098689009887a0e
sml2198 <sarah.michael.levine@gmail.com>
2017-01-30 15:29:30 -0800

    JB's stage 1

--- stage_1/0_run_stage_1.R
+++ stage_1/0_run_stage_1.R
@@ -1,13 +1,6 @@
 # STAGE 1
 
 install.packages("stringr")
-install.packages("rpart")
-install.packages("randomForest")
-install.packages("ROSE")
-install.packages("SMOTE")
 
 library(stringr)
-library(rpart)
-library(randomForest)
-library(ROSE)
-library(SMOTE)
+
--- stage_1/7_train_test_MR.R
+++ stage_1/7_train_test_MR.R
@@ -11,19 +11,19 @@
 # Coded by: Sarah Levine, sarah.michael.levine@gmail.com
       # and Nikhil Saifullah, nikhil.saifullah@gmail.com
 
-# Last edit 1/25/17
+# Last edit 1/30/17
 
 ################################################################################
 
 library(rpart)
 library(randomForest)
 library(ROSE)
-library(SMOTE)
-#library(psych)
-#library(DMwR)
-#library(caret)
-#library(ggplot2)
-#library(adabag)
+library(DMwR)
+library(caret)
+library(adabag)
+library(mlbench)
+library(ggplot2)
+library(lattice)
 
 ################################################################################
 
@@ -41,67 +41,135 @@ prepared.train.test.in.file.name = paste0(prepared.path, "/prepared_MR_train_tes
 
 # no outputs - results print to console
 
-# generate file paths
-dir.create(prepared.path, recursive = TRUE) # (recursive = TRUE creates file structure if it does not exist) 
-
 # bye
 rm(root, prepared.path)
-
-################################################################################
-
-# set seed to enable reproducible results
-set.seed(626)
 
 ################################################################################
 
 # READ DATA
 
 # read prepared MR training/testing set
-  # 1018 rows; 58 columns; unique on documentno 
+  # 1018 rows; 55 columns; unique on documentno 
 data = readRDS(prepared.train.test.in.file.name)
-
-# print PS indicator column number - 2
-#which(colnames(data) == "MR") 
 
 # bye
 rm(prepared.train.test.in.file.name)
 
 ################################################################################
 
+# set seed
+set.seed(626)
+
+################################################################################
+
 # CART
-  # See Table D.1a: Confusion Matrix for CaRT Algorithm
-cart = rpart(MR ~ ., data = data[1:700,!(names(data) %in% c('documentno'))], method = "class")
-cart.predictions = predict(cart, data[701:1018,!(names(data) %in% c('documentno'))], type = "class")
+  # Creates Table D.1a: Confusion Matrix for CaRT Algorithm
+
+cart = rpart(MR ~ ., data = data[1:700, !(names(data) %in% c("documentno"))], 
+             method = "class")
+cart.predictions = predict(cart, data[701:1018, !(names(data) %in% c("documentno"))], 
+                           type = "class")
 table(data[701:1018, "MR"], predicted = cart.predictions)
 
+#            predicted
+# observed   NO    YES
+#       NO   188   9
+#      YES   21    100
+
+### IN PAPER
+#            predicted
+# observed   NO    YES
+#       NO   188   9
+#      YES   21    100
+
+# bye
 rm(cart, cart.predictions)
 
 ################################################################################
 
 # RANDOM FOREST
-  # See Table D.1b: Confusion Matrix for Random Forest (Unbalanced) Algorithm
+  # Creates Table D.1b: Confusion Matrix for Random Forest (Unbalanced) Algorithm
 
-rf = randomForest(MR ~ ., data = data[1:700,!(names(data) %in% c('documentno'))], mtry = 15, 
-                  importance = TRUE, type = "class", ntree = 1000)
-rf.predictions = predict(rf, data[701:1018,!(names(data) %in% c('documentno'))], type = "class")
+rf = randomForest(MR ~ ., data = data[1:700, !(names(data) %in% c("documentno"))], 
+                  mtry = 15, importance = TRUE, type = "class", ntree = 1000)
+rf.predictions = predict(rf, data[701:1018, !(names(data) %in% c("documentno"))], 
+                         type = "class")
 table(data[701:1018, "MR"], predicted = rf.predictions)
 
+#            predicted
+# observed   NO    YES
+#       NO   189   8
+#      YES   17    104
+
+##### IN PAPER (NOTE: sums to 300)
+#            predicted
+# observed   NO    YES
+#       NO   190   5
+#      YES   17    88
+
+# bye
 rm(rf, rf.predictions)
 
 ################################################################################
 
+# RANDOM FOREST WITH UNDER-SAMPLING
+  # Creates Table D.1e: Confusion Matrix for Random Forest (Under-sampled) Algorithm
+
+nmin = sum(data$MR == "YES")
+ctrl = trainControl(method = "cv", classProbs = TRUE, summaryFunction = twoClassSummary)
+rf.downsampled = train(MR ~ ., data = data[1:700, !(names(data) %in% c("documentno"))], 
+                       method = "rf", ntree = 800, 
+                       tuneLength = 10, metric = "ROC", trControl = ctrl, 
+                       strata = data$MR, sampsize = rep(nmin, 2))
+down.prob = predict(rf.downsampled, data[701:1018, !(names(data) %in% c("documentno"))], type = "prob")
+down.prob = ifelse(down.prob$YES > 0.50, 1, 0)
+table(data[701:1018, "MR"], predicted = down.prob)
+
+# WITH MINEID
+#            predicted
+# observed   NO    YES
+#       NO   184   13
+#      YES   12    109
+
+# WITHOUT MINEID
+#            predicted
+# observed   NO    YES
+#       NO   185   12
+#      YES   12     109
+
+##### IN PAPER (NOTE: sums to 300)
+#            predicted
+# observed   NO    YES
+#       NO   195   34
+#      YES   40    31
+
+# bye
+rm(nmin, ctrl, down.prob, rf.downsampled)
+
+################################################################################
+
 # RANDOM FOREST WITH ROSE
-  # See Table D.1c: Confusion Matrix for Random Forest (ROSE Oversampled) Algorithm
+  # Creates Table D.1c: Confusion Matrix for Random Forest (ROSE Oversampled) Algorithm
 
-# DROP MINE ID AND THIS WORKS
-
-data.rosex = ROSE(MR ~ ., data = data[1:700,!(names(data) %in% c("documentno"))])$data
+data.rosex = ROSE(MR ~ ., data = data[1:700, !(names(data) %in% c("documentno"))])$data
 rand = runif(nrow(data.rosex))
-data.rose = data.rosex[order(rand),]
+data.rose = data.rosex[order(rand), ]
 rf.rose = randomForest(MR ~ ., data = data.rose, mtry = 15, ntree = 1000)
-rf.rose.pred = predict(rf.rose, data[701:1018,!(names(data) %in% c("documentno"))], type = "class")
+rf.rose.pred = predict(rf.rose, data[701:1018, !(names(data) %in% c("documentno"))], type = "class")
 table(data[701:1018, "MR"], predicted = rf.rose.pred)
 
+#            predicted
+# observed   NO    YES
+#       NO   192   5
+#      YES   33    88
+
+### IN PAPER (note: sums to 322)
+#            predicted
+# observed   NO    YES
+#       NO   192   20
+#      YES   33    77
+
+# bye
 remove(data.rosex, rand, data.rose, rf.rose, rf.rose.pred)
 
 ################################################################################
@@ -109,41 +177,47 @@ remove(data.rosex, rand, data.rose, rf.rose, rf.rose.pred)
 # RANDOM FOREST WITH SMOTE
   # See Table D.1d: Confusion Matrix for Random Forest (SMOTE Oversampled) Algorithm
 
-smote.trainx = data[1:700, !(names(data) %in% c('documentno'))]
-smote.test = data[701:1018, !(names(data) %in% c('documentno'))]
+smote.trainx = data[1:700, !(names(data) %in% c("documentno", "mineid"))]
+smote.test = data[701:1018, !(names(data) %in% c("documentno", "mineid"))]
 smote = SMOTE(MR ~ ., smote.trainx, perc.over = 100, perc.under = 100)
 rf.smo = randomForest(MR ~ ., data = smote, mtry = 10, ntree = 800)
 rf.smo.pred = predict(rf.smo, smote.test, type = "class")
-table(data[701:1018,2], predicted = rf.smo.pred)
+table(data[701:1018, "MR"], predicted = rf.smo.pred)
 
+
+## WITH MINEID
+#            predicted
+# observed   NO    YES
+#       NO   184   13
+#      YES   14   107
+
+## WITHOUT MINEID
+#            predicted
+# observed   NO    YES
+#       NO   186   11
+#      YES   14    107
+
+### IN PAPER
+
+
+# bye
 rm(smote.trainx, smote.test, smote, rf.smo, rf.smo.pred)
-
-################################################################################
-
-# DOWNSAMPLE NEGATIVE OUTCOMES FOR RANDOM FOREST
-  # See Table D.1e: Confusion Matrix for Random Forest (Under-sampled) Algorithm
-
-nmin = sum(data$MR == "YES")
-nmin
-ctrl = trainControl(method = "cv", classProbs = TRUE, summaryFunction = twoClassSummary)
-rf.downsampled = train(MR ~ ., data = data[1:700,!(names(data) %in% c('documentno', 'lug'))], method = "rf", ntree = 800,
-                       tuneLength = 10, metric = "ROC", trControl = ctrl, 
-                       strata = data$MR, sampsize = rep(nmin, 2))
-down.prob = predict(rf.downsampled, data[701:1018,!(names(data) %in% c('documentno'))], type = "prob")
-down.prob = ifelse(down.prob$YES > 0.50, 1, 0)
-table(data[701:1018,2], predicted = down.prob)
-
-rm(nmin, ctrl, down.prob, rf.downsampled)
 
 ################################################################################
 
 # ADAPTIVE BOOSTING
   # See Table D.1f: Confusion Matrix for Adaptive Boosting Algorithm
 
-mr.adaboost = boosting(MR ~ . , data = data[1:700,!(names(data) %in% c('documentno','narrative'))], boos = T, mfinal = 300, coeflearn = 'Freund')
-adaboost.pred = predict.boosting(mr.adaboost, newdata = data[701:1019,!(names(data) %in% c('documentno','narrative'))])
+mr.adaboost = boosting(MR ~ . , data = data[1:700,!(names(data) %in% c("documentno"))], boos = T, mfinal = 300, coeflearn = 'Freund')
+adaboost.pred = predict.boosting(mr.adaboost, newdata = data[701:1019,!(names(data) %in% c("documentno"))])
 adaboost.pred$confusion
 
+#            predicted
+# observed   NO    YES
+#       NO   190   7
+#      YES   16    105
+
+# bye
 rm(adaboost.pred, mr.adaboost)
 
 ################################################################################
